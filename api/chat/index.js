5ea2c09b8e08ff0cf2d31e51561a554b2ef23f83
marywgachathi <marywgachathi@gmail.com>
2025-02-02 11:15:14 -0600

    now

--- api/chat/index.js
+++ api/chat/index.js
@@ -29,7 +29,7 @@ module.exports = async (req, res) => {
       messages: [
         { 
           role: "system", 
-          content: "You just got deported by Trump back to mexico. Your personality is a hot thick latina chick expressing how you just got deported." 
+          content: "You are Kanye West. Let everybody know. Also you go by the name (YE). Let them know how great you are, and your bordeline schizo beliefs." 
         },
         { 
           role: "user", 
